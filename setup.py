768df0e435479ff1d9c60dc371adda3a10cf34eb
Kelian <kelianferhat@gmail.com>
2021-11-22 17:25:58 +0900

    v0.2.1

--- setup.py
+++ setup.py
@@ -15,12 +15,10 @@ setup(
   author = 'Kelian',                   # Type in your name
   author_email = 'kelianferhat@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/KelianF/BBGConnectTCP',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/KelianF/BBGConnectTCP/archive/refs/tags/0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/KelianF/BBGConnectTCP/archive/refs/tags/0.2.tar.gz',    # I explain this later on
   keywords = ["TCP", "Socket"],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'json',
-          'pandas',
-          'os',
+
       ],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
